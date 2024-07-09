ea9e36a47307149b0a41738514ab99a985561f11
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-09 16:38:01 -0300

    feat(routes): add get-links route handler

---
+++ src/routes/get-links.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+const schema = {
+  params: z.object({
+    tripID: z.string().uuid(),
+  }),
+};
+
+export async function getTripLinks(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get("/trips/:tripID/links", { schema }, async (req, res) => {
+      const { tripID } = req.params;
+      const trip = await prisma.trip.findUnique({
+        where: { id: tripID },
+        include: {
+          links: true,
+        },
+      });
+
+      if (!trip) throw new Error("Trip not found.");
+
+      return { links: trip.links };
+    });
+}
