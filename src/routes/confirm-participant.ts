0d70f9b88807d1f15f1f87b24602d0dc575ee535
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-09 16:37:07 -0300

    feat(routes): add confirm-participant route handler

---
+++ src/routes/confirm-participant.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+const schema = { params: z.object({ participantID: z.string().uuid() }) };
+
+export async function confirmParticipant(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get("/participants/:participantID/confirm", { schema }, async (req, res) => {
+      const { participantID } = req.params;
+      const participant = await prisma.participant.findUnique({ where: { id: participantID } });
+
+      if (!participant) throw new Error("Participant not found.");
+
+      if (!participant.is_confirmed) {
+        await prisma.participant.update({
+          where: { id: participant.id },
+          data: {
+            is_confirmed: true,
+          },
+        });
+      }
+
+      return res.redirect(String(process.env.WEB_URL));
+    });
+}
