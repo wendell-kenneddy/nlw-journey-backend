f65b3153b13f3b7c4b860474bf7fd93f8798f909
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-09 16:38:22 -0300

    feat(server.ts): register new routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
+import cors from "@fastify/cors";
 import fastify from "fastify";
-import { createTrip } from "./routes/create-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmParticipant } from "./routes/confirm-participant";
+import { confirmTrip } from "./routes/confirm-trip";
+import { createTrip } from "./routes/create-trip";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createTripLink } from "./routes/create-link";
+import { getTripLinks } from "./routes/get-links";
 
 const app = fastify();
 const port = Number(process.env.PORT);
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+app.register(cors, { origin: String(process.env.ORIGIN) });
+
 app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createTripLink);
+app.register(getTripLinks);
 
 app.listen({ port }, () => {
   console.log("[server]: server running on http://localhost:" + port);
